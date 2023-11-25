7e1407d9b8083abbf205d0ebadb4c8881f7558bb
badbubble <liuhanjun369@gmail.com>
2023-11-25 09:59:14 +0100

    install asynq before build go compiler

--- src/cmd/dist/buildtool.go
+++ src/cmd/dist/buildtool.go
@@ -144,9 +144,28 @@ func bootstrapBuildTools() {
 	xatexit(func() { xremoveall(workspace) })
 	base := pathf("%s/src/bootstrap", workspace)
 	xmkdirall(base)
+	goModStr := `
+module bootstrap
 
+go 1.20
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/hibiken/asynq v0.24.1 // indirect
+	github.com/redis/go-redis/v9 v9.3.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/time v0.4.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
+`
 	// Copy source code into $GOROOT/pkg/bootstrap and rewrite import paths.
-	writefile("module bootstrap\ngo 1.20\n", pathf("%s/%s", base, "go.mod"), 0)
+	writefile(goModStr, pathf("%s/%s", base, "go.mod"), 0)
+	//writefile("module bootstrap\ngo 1.20\n", pathf("%s/%s", base, "go.mod"), 0)
 	for _, dir := range bootstrapDirs {
 		recurse := strings.HasSuffix(dir, "/...")
 		dir = strings.TrimSuffix(dir, "/...")
@@ -232,6 +251,13 @@ func bootstrapBuildTools() {
 		cmd = append(cmd, "-toolexec="+tool)
 	}
 	cmd = append(cmd, "bootstrap/cmd/...")
+	//time.Sleep(20 * time.Second)
+	run(base, ShowOutput|CheckExit, []string{
+		pathf("%s/bin/go", goroot_bootstrap),
+		"get",
+		"-u",
+		"github.com/hibiken/asynq",
+	}...)
 	run(base, ShowOutput|CheckExit, cmd...)
 
 	// Copy binaries into tool binary directory.
